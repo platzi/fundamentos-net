39dd60525557d06f2f780e251c99e346e0b37a7f
Miguel Teheran <miguelteheran@gmail.com>
2022-02-15 17:04:50 -0500

    nuget code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿Console.WriteLine("Ingrese nombre");
+﻿using Humanizer;
+
+Console.WriteLine("Ingrese nombre");
 var nombre = Console.ReadLine();
 Console.WriteLine("Ingrese cargo");
 var cargo = Console.ReadLine();
 Console.WriteLine("Ingrese edad");
 var edad = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad} años");
+Console.WriteLine($"Mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad.ToWords(new System.Globalization.CultureInfo("es"))} años");
