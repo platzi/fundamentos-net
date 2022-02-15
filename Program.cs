e7d45923d0af569e29878ac834b6e940827ea95b
Miguel Teheran <miguelteheran@gmail.com>
2022-02-15 16:58:58 -0500

    adding globaljson

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ingrese nombre");
+﻿Console.WriteLine("Ingrese nombre");
 var nombre = Console.ReadLine();
 Console.WriteLine("Ingrese cargo");
 var cargo = Console.ReadLine();
