5139ec4d5582e610d6019aaaefb7a1937c945494
Miguel Teheran <miguelteheran@gmail.com>
2022-02-15 15:56:15 -0500

    base file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ingrese nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("Ingrese cargo");
+var cargo = Console.ReadLine();
+Console.WriteLine("Ingrese edad");
+var edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad} años");
